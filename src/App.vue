58dfe894eaf794305159322b57a208df02ddd577
SamuelePassiatore <semmisno2@protonmail.com>
2023-01-19 16:03:02 +0100

    add app title component to app.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- JS -->
+<script>
+import AppTitle from './components/AppTitle.vue';
+export default {
+    components: {
+        AppTitle
+    }
+}
+</script>
+
+<!-- HTML -->
+<template>
+    <app-title></app-title>
+</template>
+
+<!-- CSS -->
+<style>
+
+</style>
